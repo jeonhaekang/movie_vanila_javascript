b174f7cb146acb02c9abb4c2aeea5a691cec76b7
jeonhaekang <1003gorkd@naver.com>
2023-06-05 20:14:45 +0900

    chore: 상세 페이지 설명 추가

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -17,7 +17,10 @@ export class DetailPage extends Page {
     const movieId = params.get("movieId");
     const movieInfo = await getDetailMovie(movieId);
 
-    const { poster_path, title } = movieInfo;
+    const { poster_path, title, vote_average, release_date, runtime, overview, tagline } =
+      movieInfo;
+
+    console.log(movieInfo);
 
     const detailInfo = document.querySelector(".detail");
     detailInfo.innerHTML = `
@@ -28,7 +31,29 @@ export class DetailPage extends Page {
       </div>
 
       <div class="detail-info">
-        <h2>${title}</h2>
+        <h2 class="detail-title">${title}</h2>
+
+        <div class="detail-rating">
+        </div>
+
+        <div class="detail-etc-box">
+          <span class="detail-score">평점 ${vote_average}</span>
+          
+          <span class="detail-release-date">개봉일 ${release_date}</span>
+
+          <span class="detail-runtime">상영시간 ${runtime}분</span>
+        </div>
+
+        <div class="detail-desc-box">
+          <dl class="detail-desc">
+            <dt>요약</dt>
+            <dd>${tagline}</dd>
+          </dl>
+          <dl class="detail-desc">
+            <dt>소개</dt>
+            <dd>${overview}</dd>
+          </dl>
+        </div>
       </div>
     `;
   }
