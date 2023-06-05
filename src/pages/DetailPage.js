f5f6050dc53dcda544c28b183786836a0d1a8961
jeonhaekang <1003gorkd@naver.com>
2023-06-05 19:05:37 +0900

    feat: 상세페이지 포스터 생성

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,20 +1,31 @@
 import { getDetailMovie } from "../apis/movie.js";
+import { IMAGE_BASE_URL } from "../constants.js";
 import { Page } from "./Page.js";
 
 export class DetailPage extends Page {
   constructor() {
-    super(/*html*/ `
-        <main>
-          <div>디테일페이지</div>
-        </main>
+    super(`
+      <div class="detail-container">
+        <main class="detail"></main>
+      </div>
     `);
   }
 
-  onFinally() {
+  async onFinally() {
     const params = new URLSearchParams(location.search);
 
     const movieId = params.get("movieId");
+    const movieInfo = await getDetailMovie(movieId);
 
-    getDetailMovie(movieId);
+    const { poster_path } = movieInfo;
+
+    const detailInfo = document.querySelector(".detail");
+    detailInfo.innerHTML = `
+      <div class="detail-poster">
+        <figure style="background-image: url(${IMAGE_BASE_URL}/${poster_path})">
+          <img src="${IMAGE_BASE_URL}/${poster_path}"/>
+        </figure>        
+      </div>
+    `;
   }
 }
